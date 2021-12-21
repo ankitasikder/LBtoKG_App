f399acc811638a83068697caf36bc00f6befea61
ankitasikder <79798763+ankitasikder@users.noreply.github.com>
2021-12-21 20:41:40 +0530

    added by ankita

---
+++ app/src/main/java/com/example/lbtokg/MainActivity.java
@@ -0,0 +1,49 @@
+package com.example.lbtokg;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    EditText lbIn;
+    Button submit;
+    TextView kg;
+    ImageView iv;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        lbIn=(EditText) findViewById(R.id.lbIn);
+        submit=(Button)findViewById(R.id.button);
+        kg=(TextView)findViewById(R.id.output);
+        iv=(ImageView)findViewById(R.id.lbImg);
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(lbIn.getText().toString().isEmpty()){
+                    kg.setText("Empty Input Value\nTry Again");
+
+                }
+                else{
+                    Log.d("input",lbIn.getText().toString()+"lbs");
+                    Log.d("output",(Double.parseDouble(lbIn.getText().toString())*0.453592)+"kg");
+                    kg.setText("-: KG VALUE :- \n"+String.format("%.2f",(Double.parseDouble(lbIn.getText().toString())*0.453592))+"kg");
+                    iv.setImageResource(R.drawable.pok);
+                }
+
+            }
+        });
+
+    }
+    public void onClickOnKgIn(View view){
+        Log.d("Response","Clicked lbs Input Editable Text ");
+        iv=(ImageView)findViewById(R.id.lbImg);
+        iv.setImageResource(R.drawable.lbpok);
+    }
+}
